8d5d687e0ee88be059e94cbe446a77bbdd2e0eed
Matthew Bennett <BennettMatthew@JohnDeere.com>
2023-07-25 11:52:10 -0500

    correct navbar still needs to have finished on hover effects

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,15 +1,28 @@
-// components/CyberpunkNavBar.js
-
 import React from "react";
 import "../componentcss/navbar.css";
 import Link from "next/link";
+
 const NavBar = () => {
   return (
     <div className='navbar'>
-      <Link href='/'>Home</Link>
-      <Link href='/projects'>Projects</Link>
-      <Link href='/resume'>Resume</Link>
-      <Link href='/contact'>Contact</Link>
+      {/* Red rectangle */}
+      <div className='red-rectangle'></div>
+      {/* Red triangle */}
+      <div className='red-triangle'></div>
+      {/* Blue parallelogram shapes */}
+      <div className='link'>
+        <Link href='/'>Home</Link>
+      </div>
+      <div className='link'>
+        <Link href='/projects'>Projects</Link>
+      </div>
+      <div className='link'>
+        <Link href='/resume'>Resume</Link>
+      </div>
+      <div className='link'>
+        <Link href='/contact'>Contact</Link>
+      </div>
+      <div className='empty-rhombus'></div>
     </div>
   );
 };
