389f138b4b4c71fe1f12bc0749263d1278a8a2dc
kevin21018212 <64773820+kevin21018212@users.noreply.github.com>
2023-08-12 14:44:59 -0500

    added a clust section to landing and got the wave to work

--- app/components/landing.tsx
+++ app/components/landing.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import {TypeAnimation} from "react-type-animation";
 import "../componentcss/landing.css";
+import Wave from "react-wavify";
+isFinite
 const Landing = () => {
   return (
     <div className='landing-container'>
@@ -13,6 +15,9 @@ const Landing = () => {
         <div className='landing-typewriter'>
           <TypeAnimation sequence={["Developer", 3000, "Programmer", 3000, "Designer", 3000]} wrapper='span' speed={10} repeat={Infinity} />
         </div>
+      </div>
+      <div className="landing-cluster">
+      <Wave fill='#32CD32' paused={false} options={{ height: 100,amplitude: 20, speed: 0.1, points: 3}}/>
       </div>
     </div>
   );
