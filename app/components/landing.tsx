236a338c6dad8b054aa95afe907bb48da550296d
kevin21018212 <64773820+kevin21018212@users.noreply.github.com>
2023-08-12 16:54:59 -0500

    css complete for github tracking component still needs photoshop finish, moved some files around for easier access

--- app/components/landing.tsx
+++ app/components/landing.tsx
@@ -7,40 +7,44 @@ import Wave from "react-wavify";
 
 
 
-const fetchWeeklyContributions = async (username:string) => {
-  const today = new Date();
-  const oneWeekAgo = new Date();
-  oneWeekAgo.setDate(today.getDate() - 7); // Calculate date one week ago
 
-  const accessToken = process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN;
-
-  const response = await fetch(`https://api.github.com/search/commits?q=author:${username}+author-date:${oneWeekAgo.toISOString()}..${today.toISOString()}`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-
-  const data = await response.json();
-  const contributionCount = data.total_count;
-
-  return contributionCount;
-};
 
 const Landing = () => {
+  const [amplitude, setAmplitude] = useState(0);
 
+  useEffect(() => {
+    const username = 'kevin21018212'; 
+    const accessToken = process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN;
+
+    const fetchContributions = async () => {
+      try {
+        const today = new Date();
+        const oneWeekAgo = new Date();
+        oneWeekAgo.setDate(today.getDate() - 7);
+
+        const response = await fetch(`https://api.github.com/users/${username}/events`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const events = await response.json();
+        const pushesThisWeek = events.filter((event: { created_at: string | number | Date; type: string; }) => {
+          const eventDate = new Date(event.created_at);
+          return event.type === 'PushEvent' && eventDate >= oneWeekAgo;
+        });
+
+        setAmplitude(pushesThisWeek.length);
+      } catch (error) {
+        console.error('Error fetching GitHub pushes:', error);
+      }
+    };
+
+    fetchContributions();
+  }, []);
  
-  const username = 'kevin21018212'; // Replace with your GitHub username
-
-  fetchWeeklyContributions(username)
-    .then((contributions) => {
-      console.log(`This week's contributions: ${contributions}`);
-    })
-    .catch((error) => {
-      console.error('Error fetching contributions:', error);
-    });
-
-
   return (
+    
     <div className='landing-container'>
       <div className='landing-content'>
         <div className='landing-title'>
@@ -51,8 +55,11 @@ const Landing = () => {
         </div>
       </div>
       <div className="landing-cluster">
-      <Wave fill='#32CD32' paused={false} options={{ height: 100,amplitude: 20, speed: 0.1, points: 3}}/>
+      <div className="wave-text">This week:  {amplitude}</div>
+      <div className="wave">
+      <Wave fill='#32CD32' paused={false} options={{ height: 30,amplitude:(amplitude*8), speed: 0.1, points: 3}}/>
       </div>
+    </div>
     </div>
   );
 };
