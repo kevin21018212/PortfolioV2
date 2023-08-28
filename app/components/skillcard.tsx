fc1c63221411a09b0192e4890aa6d1c6c3e4b16c
kevin21018212 <64773820+kevin21018212@users.noreply.github.com>
2023-08-28 12:53:49 -0500

    progress on skill cards

--- app/components/skillcard.tsx
+++ app/components/skillcard.tsx
@@ -1,6 +1,15 @@
+import "../componentcss/skillcard.css";
 const SkillCard = () => {
     return(
         <div className="skill-card">
+            <div>
+           <p className="textlarge">Front-End</p>
+           </div>
+           <div><p className="textmedium">Internship, John Deere 1yr</p></div>
+           <div className="card-middle">
+             <div className="image"></div>
+            <div className="text"><p className="textsmall">JavaScript,HTML,Css,TypeScript,NextJS 13</p></div>
+           </div>
        </div>
     )
 }
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,19 +1,13 @@
+import SkillCard from "../components/skillcard";
 import Spacer from "../components/spacer";
 import './resume.css'
 const ResumePage = () => {
     return (
-      <div className="page">
-        <Spacer title="Skills"/>
-        <div className="skills-section">
-            
-        </div>
-        <Spacer title="Experience"/>
-        <div className="experince-section"></div>
-        <Spacer title="Classes"/>
-        <div className="classes"></div>
-        <Spacer title="Extra"/>
-        <div className="extracricular"></div>
-      </div>
+    
+        <><Spacer title="Skills" /><div className="skills-section">
+        <SkillCard />
+      </div><Spacer title="Experience" /><div className="experince-section"></div><Spacer title="Classes" /><div className="classes"></div><Spacer title="Extra" /><div className="extracricular"></div></>
+     
     )
 }
 export default ResumePage;
