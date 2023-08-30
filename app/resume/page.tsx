c7d094d0e509d71166b5f4f2c876e8e9b6668294
kevin21018212 <64773820+kevin21018212@users.noreply.github.com>
2023-08-30 13:20:11 -0500

    Skillcard Section

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,12 +1,26 @@
 import SkillCard from "../components/skillcard";
 import Spacer from "../components/spacer";
 import './resume.css'
+
+
 const ResumePage = () => {
+
+
+  
+
     return (
     
-        <><Spacer title="Skills" /><div className="skills-section">
-        <SkillCard />
-      </div><Spacer title="Experience" /><div className="experince-section"></div><Spacer title="Classes" /><div className="classes"></div><Spacer title="Extra" /><div className="extracricular"></div></>
+      <>
+        <Spacer title="Skills" /><div className="skills-section">
+         <><SkillCard /><SkillCard /><SkillCard /><SkillCard /><SkillCard /></>
+        </div>
+        <Spacer title="Experience" />
+        <div className="experince-section"></div>
+        <Spacer title="Classes" />
+        <div className="classes"></div>
+        <Spacer title="Extra" />
+        <div className="extracricular"></div>
+      </>
      
     )
 }
