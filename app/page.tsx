fac17016b17054fb847fb509bf8ed49c129a4133
kevin21018212 <trashlinkmain@gmail.com>
2023-11-04 12:57:02 -0500

    fix:project toggle spacing and general page spacing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 import styles from "./page.module.css";
-
-import {useEffect, useState} from "react";
-import LoadingAnimation from "./Homepage/components/loading";
 import Landing from "./Homepage/components/landing/landing";
 import Project from "./Homepage/components/projects/project";
 
@@ -10,25 +7,10 @@ export default function Home() {
 
 
 
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const loadingTimer = setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(loadingTimer);
-  }, []);
 
   return (
-    <div className={styles.container}>
-      {isLoading ? (
-        <LoadingAnimation />
-      ) : (
-        <>
 
-
-
+     
           <div className={styles.maincontent}>
            <div className={styles.landingcontainer}>
             <Landing />
@@ -37,8 +19,5 @@ export default function Home() {
             <Project/>
             </div>
           </div>
-        </>
-      )}
-    </div>
   );
 }
