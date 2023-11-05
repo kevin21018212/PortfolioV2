f45cba2b29002b1c87d1ee50d57284416c208154
kevin21018212 <trashlinkmain@gmail.com>
2023-11-05 11:50:53 -0600

    animations for projects

--- app/Homepage/components/projects/projectinfo.tsx
+++ app/Homepage/components/projects/projectinfo.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import React, { useState, useEffect } from "react";
-import "../../css/projects/projectinfo.css"
+import "../../css/projects/projectinfo.css";
 
 // Define a type for project data
 type ProjectData = {
@@ -12,33 +12,78 @@ type ProjectData = {
   bottomMiddle: string;
 };
 
-const ProjectInfo: React.FC<{ projectData: ProjectData | null }> = ({ projectData }) => {
-  // State variable to track changes in projectData
+const textanimation = {
+  inactive: {
+    opacity: 0,
+    translateX: "-5vh", // Change to -5vh for left, 5vh for middle
+  },
+  active: {
+    opacity: 1,
+    translateX: "0vh", // Change to 0vh for left, 5vh for middle
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.2, // Adjust the stagger delay as needed.
+    },
+  },
+};
 
-  function handleTextAnimation() {}
+const staggerChildrenAnimation = {
+  active: {
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.2, // Adjust the stagger delay as needed.
+    },
+  },
+};
+
+const ProjectInfo = ({ projectData }: { projectData: ProjectData }) => {
+  const [animationState, setAnimationState] = useState("inactive");
 
   useEffect(() => {
-    if (projectData) {
-      handleTextAnimation();
-    }
+    setAnimationState("inactive");
 
-    console.log(projectData);
+    setTimeout(() => {
+      setAnimationState("active");
+    }, 150);
   }, [projectData]);
 
   return (
-    <div className='projects-content'>
-      {projectData !== null && (
+    <div className="projects-content">
+      {projectData && (
         <>
-          <div className='projects-content-right'>
-            <motion.p className={`textsmall`}>{projectData.topRight}</motion.p>
-            <motion.p className={`textlarge`}>{projectData.middleRight}</motion.p>
-            <motion.p className={`textsmall`}>{projectData.bottomRight}</motion.p>
-          </div>
-          <div className='projects-content-left'>
-            <motion.p className={`texttitle`}>Idea</motion.p>
-            <motion.p className={`textxmedium`}>{projectData.middleMiddle}</motion.p>
-            <motion.p className={`textxsmall`}>{projectData.bottomMiddle}</motion.p>
-          </div>
+          <motion.div
+            variants={staggerChildrenAnimation}
+            initial="inactive"
+            animate={animationState}
+            exit="inactive"
+            className="projects-content-left"
+          >
+            <motion.p className={`textsmall`} variants={textanimation}>
+              {projectData.topRight}
+            </motion.p>
+            <motion.p className={`textlarge`} variants={textanimation}>
+              {projectData.middleRight}
+            </motion.p>
+            <motion.p variants={textanimation}>
+              {projectData.bottomRight}
+            </motion.p>
+          </motion.div>
+          <motion.div
+            variants={staggerChildrenAnimation}
+            initial="inactive"
+            animate={animationState}
+            exit="inactive"
+            className="projects-content-middle"
+          >
+            <p>Idea</p>
+            <motion.p variants={textanimation}>
+              {projectData.middleMiddle}
+            </motion.p>
+            <motion.p variants={textanimation}>
+              {projectData.bottomMiddle}
+            </motion.p>
+          </motion.div>
+          <div className="projects-content-right"></div>
         </>
       )}
     </div>
