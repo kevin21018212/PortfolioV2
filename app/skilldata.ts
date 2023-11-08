74f9259fb35461c8b90f20dc046a766d438a1638
kevin21018212 <trashlinkmain@gmail.com>
2023-11-07 23:36:27 -0600

    card

--- app/skilldata.ts
+++ app/skilldata.ts
@@ -1,10 +1,8 @@
-// skillsData.ts
-
 interface SkillData {
   title: string;
   skillName: string;
   experience: string;
-  backgroundImageUrl: string;
+  backgroundImageUrl: string | null;
 }
 
 const skillsData: SkillData[] = [
@@ -12,73 +10,73 @@ const skillsData: SkillData[] = [
     title: "CLOUD",
     skillName: "AWS",
     experience: "2 years, Full-time",
-    backgroundImageUrl: "/projectimg/aws.jpeg", // Updated to be relative to the public folder
+    backgroundImageUrl: "/projectimg/aws.jpeg",
   },
   {
     title: "FRONTEND",
     skillName: "Next.js",
     experience: "1 year, Full-time",
-    backgroundImageUrl: "/projectimg/nextjs.jpeg", // Updated to be relative to the public folder
+    backgroundImageUrl: "/projectimg/nextjs.jpeg",
   },
   {
     title: "BACKEND",
     skillName: "Node.js",
     experience: "3 years, Full-time",
-    backgroundImageUrl: "/projectimg/node.jpeg", // Updated to be relative to the public folder
+    backgroundImageUrl: "/projectimg/node.jpeg",
   },
   {
     title: "DATABASE",
     skillName: "SQL",
     experience: "4 years, Full-time",
-    backgroundImageUrl: "url_to_sql_background_image.jpg",
+    backgroundImageUrl: null, // Set to null
   },
   {
     title: "FRONTEND",
     skillName: "React",
     experience: "2 years, Full-time",
-    backgroundImageUrl: "url_to_react_background_image.jpg",
+    backgroundImageUrl: null, // Set to null
   },
   {
     title: "PROGRAMMING",
     skillName: "C++",
     experience: "5 years, Full-time",
-    backgroundImageUrl: "/projectimg/c++.png", // Updated to be relative to the public folder
+    backgroundImageUrl: "/projectimg/c++.png",
   },
   {
     title: "PROGRAMMING",
     skillName: "C",
     experience: "3 years, Full-time",
-    backgroundImageUrl: "url_to_c_background_image.jpg",
+    backgroundImageUrl: null, // Set to null
   },
   {
     title: "PROGRAMMING",
     skillName: "Java",
     experience: "4 years, Full-time",
-    backgroundImageUrl: "/projectimg/java.jpeg", // Updated to be relative to the public folder
+    backgroundImageUrl: "/projectimg/java.jpeg",
   },
   {
     title: "DATABASE",
     skillName: "PrismaDB",
     experience: "1 year, Full-time",
-    backgroundImageUrl: "url_to_prismadb_background_image.jpg",
+    backgroundImageUrl: null, // Set to null
   },
   {
     title: "FRONTEND",
     skillName: "CSS",
     experience: "3 years, Full-time",
-    backgroundImageUrl: "url_to_css_background_image.jpg",
+    backgroundImageUrl: null, // Set to null
   },
   {
     title: "PROGRAMMING",
     skillName: "Data Structures and Algorithms",
     experience: "4 years, Full-time",
-    backgroundImageUrl: "url_to_ds_algo_background_image.jpg",
+    backgroundImageUrl: null, // Set to null
   },
   {
     title: "TOOLS",
     skillName: "Git",
     experience: "2 years, Full-time",
-    backgroundImageUrl: "url_to_git_background_image.jpg",
+    backgroundImageUrl: null, // Set to null
   },
 ];
 
