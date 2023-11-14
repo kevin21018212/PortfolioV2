79a4d413de7b0c7ee80b20c6ce902c5556738479
kevin21018212 <trashlinkmain@gmail.com>
2023-11-14 14:45:26 -0600

    fixed filename conflict

--- app/Homepage/components/landing/landing.tsx
+++ app/Homepage/components/landing/landing.tsx
@@ -1,21 +1,15 @@
-// components/Landing.js
 "use client";
 import React, { useEffect, useState } from "react";
 
 import "../../css/landing/landing.css";
-
-import LandingText from "./landingtext";
 import LandingCluster from "./landingcluster";
-
-
-
-
+import LandingText from "./landingtext";
 
 const Landing = () => {
   const [amplitude, setAmplitude] = useState(0);
 
   useEffect(() => {
-    const username = 'kevin21018212'; 
+    const username = "kevin21018212";
     const accessToken = process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN;
 
     const fetchContributions = async () => {
@@ -24,32 +18,36 @@ const Landing = () => {
         const oneWeekAgo = new Date();
         oneWeekAgo.setDate(today.getDate() - 7);
 
-        const response = await fetch(`https://api.github.com/users/${username}/events`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
+        const response = await fetch(
+          `https://api.github.com/users/${username}/events`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
 
         const events = await response.json();
-        const pushesThisWeek = events.filter((event: { created_at: string | number | Date; type: string; }) => {
-          const eventDate = new Date(event.created_at);
-          return event.type === 'PushEvent' && eventDate >= oneWeekAgo;
-        });
+        const pushesThisWeek = events.filter(
+          (event: { created_at: string | number | Date; type: string }) => {
+            const eventDate = new Date(event.created_at);
+            return event.type === "PushEvent" && eventDate >= oneWeekAgo;
+          }
+        );
 
         setAmplitude(pushesThisWeek.length);
       } catch (error) {
-        console.error('Error fetching GitHub pushes:', error);
+        console.error("Error fetching GitHub pushes:", error);
       }
     };
 
     fetchContributions();
   }, []);
- 
+
   return (
     <div className="landing-container">
-     <LandingText/>
-     <LandingCluster amplitude={amplitude}/>
-     
+      <LandingText />
+      <LandingCluster amplitude={amplitude} />
     </div>
   );
 };
